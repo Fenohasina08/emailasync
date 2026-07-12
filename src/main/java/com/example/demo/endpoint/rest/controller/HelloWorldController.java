df93b9ccd2445af65708329e71722448cb9b48ec
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-12 21:26:47 +0300

    update(HelloWorldController): produce an event instead of directly sending for the email

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,8 +1,8 @@
 package com.example.demo.endpoint.rest.controller;
 
-import com.example.demo.mail.Email;
-import com.example.demo.mail.Mailer;
-import jakarta.mail.internet.InternetAddress;
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
+
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,15 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final Mailer mailer;
+    private final EventProducer<SendEmailRequested> eventProducer;
 
     @GetMapping("/hello")
     @SneakyThrows
     public String helloWorld(@RequestParam String to) {
-        var email =
-                new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
-
-        mailer.accept(email);
+        var event = SendEmailRequested.builder().to(to).build();
+        eventProducer.accept(List.of(event));
         return "... world!";
     }
 }
